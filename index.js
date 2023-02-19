f9ce89350f9a8ed9b46b2b77d86c742de2222430
eakbarib <eakbarib@sfu.ca>
2023-02-19 12:11:06 -0800

    Version 1 of app

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const ejs = require('ejs');
 const app = express();
-const axios = require('axios');
 const Question = require('./models/question');
 
 // Constants
@@ -13,13 +11,13 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
+
+
 // Set up a route to fetch data from OpenAI API and render the response using EJS
 app.get('/', async (req, res) => {
     try {
-    const questions_raw = await Question.getQuestions();
-    const questions = [];
     // Render the response using EJS
-    res.render('index', { data: {questions} });
+    res.render('index');
     //correct_language:question.language,sentence:question.sentence,translate:question.translate,language_options:question.choices
     } 
     catch (error) {
@@ -28,8 +26,22 @@ app.get('/', async (req, res) => {
     }
 });
 
+app.get('/q/:id', async (req, res) => {
+    try {
+        const question = await Question.getQuestion(req.params.id);
+        // Render the response using EJS
+        //correct_language:question.language,sentence:question.sentence,translate:question.translate,language_options:question.choices
+        res.send(question); 
+    } 
+        catch (error) {
+            console.error(error);
+            res.send('Error fetching data');
+    }
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
+
 
--- models/question.js
+++ models/question.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
-const json = require('json');
 
-async function getQuestions() {
+async function getQuestion(id) {
     try {
-        response = await axios.get('https://foundintranslation-8863a-default-rtdb.firebaseio.com/questions.json');
+        response = await axios.get('https://foundintranslation-8863a-default-rtdb.firebaseio.com/questions/question'+id+'.json');
         return response.data;
     } catch (error) {
         console.error(error);
     }
 }
 
-exports.getQuestions = getQuestions;
+exports.getQuestion = getQuestion;
